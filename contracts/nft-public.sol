2865e888170b3ac7b8902b6b1eb304ede93fbf9d
svinerus <svinerus@gmail.com>
2021-11-03 18:43:16 +0200

    Add public nft contract

---
+++ contracts/nft-public.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract nftPublic is ERC1155 {
+
+    address immutable marketplace;
+
+    constructor(address marketplace_, string memory uri) ERC1155(uri) {
+        marketplace = marketplace_;
+    }
+
+    function isApprovedForAll(address account, address operator) public view virtual override returns (bool) {
+        if (operator == marketplace) return true;
+        return super.isApprovedForAll(account, operator);
+    }
+
+}
