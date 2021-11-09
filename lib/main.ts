c7f443e21e689413e3ed4c2beb2a487d9233c3dd
svinerus <svinerus@gmail.com>
2021-11-09 19:03:22 +0200

    Throw if no env vars

--- lib/main.ts
+++ lib/main.ts
@@ -7,8 +7,11 @@ import {start} from "./api/api";
 async function main() {
   dotenv.config();
 
+  if (!process.env.PRIVATEKEY) throw "No PRIVATEKEY env var"
+  if (!process.env.INFURA_KEY) throw "No INFURA_KEY env var"
+
   const provider = new ethers.providers.InfuraProvider('rinkeby', process.env.INFURA_KEY)
-  const signer = new ethers.Wallet(process.env.PRIVATEKEY || "", provider)
+  const signer = new ethers.Wallet(process.env.PRIVATEKEY, provider)
   const contract = ethers.ContractFactory.getContract(deployment.address, deployment.abi, signer)
   const marketplace = new Marketplace(contract);
   await start(marketplace);
