d772adf4483ef19a9b80530fb0e03d1f73274b01
kiwvic <you@example.com>
2021-11-11 20:14:32 +0200

    Added graphql objects

--- lib/api/schema.ts
+++ lib/api/schema.ts
@@ -1,10 +1,58 @@
-import { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList, GraphQLSchema } from "graphql";
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLScalarType
+} from "graphql";
+
+import {TokensCollection} from "../types/mongo";
+
+const DictOwnersType = new GraphQLScalarType({
+  name: "DictOwners",
+  serialize(val){
+    // send value as response
+    return val
+  }
+});
+
+
+const PseudoTokenType = new GraphQLObjectType({
+  name: "PseudoToken",
+  fields: () => ({
+    tokenId: { type: GraphQLString },
+    metadata_: { type: GraphQLString },
+    owners: {
+      type: DictOwnersType
+    }
+  })
+});
+
+
+const CollectionTokensType = new GraphQLObjectType({
+  name: "CollectionTokens",
+  fields: () => ({
+    contractAddress: { type: GraphQLString },
+    tokenType: { type: GraphQLInt },
+    owner: { type: GraphQLString },
+    tokens: {
+      type: new GraphQLList(PseudoTokenType),
+    }
+  })
+});
 
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
-  fields: {  }
+  fields: () => ({
+    collectionsTokens: {
+      type: new GraphQLList(CollectionTokensType),
+      resolve: () => { return TokensCollection.find(); }
+    }
+  })
 });
+
 
 export const schema = new GraphQLSchema({
   query: RootQuery,
